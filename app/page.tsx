7767a7cd774ab69f348e39643ad7862e94464320
TimPlaudis <nvk97174@gmail.com>
2026-04-17 13:38:59 +0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Medical Registry Kazakhstan</h1>
+      <p>Project started successfully 🚀</p>
+    </div>
+  );
+}
